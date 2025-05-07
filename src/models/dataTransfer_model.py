1c57e848ec02b77bb31390a42337ecaa380dd518
Marclova <cocilovamarco01@gmail.com>
2025-05-07 18:02:19 +0200

    [CREATE][UPDATE] Modified the embedding storing format for vectorial DB implementation. Furthermore the data transfer model has been defined and implemented into the manager in order to ease row data management.

---
+++ src/models/dataTransfer_model.py
@@ -0,0 +1,56 @@
+class DT_model:
+    """
+    Data Transfer model for storing and retrieving data from a vectorial database.
+    """
+    def __init__(self, url: str, title: str = "untitled", authors: list[str] = ["unknown"], 
+                 id: str = None, vector: list[float] = list(), text: str = None, embedder_name: str = None):
+        """Initializes a data transfer model for storing and retrieving data from a vectorial database."""
+        if (url == None or title == None):
+            raise ValueError("URL and title cannot be None")
+        if (title == ""):
+            title = "untitled"
+        if (authors.__len__() == 0):
+            authors = ["unknown"]
+        if (vector != None and vector.__len__() > 0):
+            if (text == None):
+                raise ValueError("Text cannot be None if vector is provided")
+            if (embedder_name == None):
+                raise ValueError("Embedder cannot be None if vector is provided")
+        
+        self.id = id
+        self.vector = vector
+        self.text = text
+        self.url = url
+        self.title = title
+        self.authors = authors
+        self.embedder_name = embedder_name
+
+
+
+    def update_vector(self, vector: list[float], text: str, embedder_name: str) -> None:
+        """Updates the vector, text and embedder of the object."""
+        if (vector == None or vector.__len__() == 0):
+            raise ValueError("Vector cannot be None or empty")
+        if (text == None):
+            raise ValueError("Text cannot be None")
+        if (embedder_name == None):
+            raise ValueError("Embedder cannot be None")
+        
+        self.vector = vector
+        self.text = text
+        self.embedder_name = embedder_name
+
+    
+    def generate_JSON_data(self) -> dict[str, any]:
+        """Returns a dictionary representation of the object for vectorial database storage."""
+        return {
+            "id": self.id,
+            "vector": self.vector,
+            "text": self.text,
+            "metadata": {
+                "title": self.title,
+                "authors": self.authors,
+                "url": self.url,
+                "embedder": self.embedder_name
+            }
+        }
--- src/services/DB_operator.py
+++ src/services/DB_operator.py
@@ -5,7 +5,7 @@ class MongoDB_manager:
     """
     Class to manage the MongoDB connection and operations.
     """
-
+    #TODO add connection checking
     def __init__(self, DB_connection_url: str, DB_name: str):
         self.connection = MongoClient(DB_connection_url)
         self.database = self.connection[DB_name]
@@ -14,31 +14,43 @@ class MongoDB_manager:
         # self.collection.create_index("title", unique=True)
 
 
-    def get_all_titleURL_couples(self, input_collection_name: str, title_normalization: bool = True) -> dict[str,str]:
+    # def get_all_titleURL_couples(self, input_collection_name: str, title_normalization: bool = True) -> dict[str,str]:
+    #     """
+    #     Retrieves all the records in the given Mongo collection.
+
+    #     Parameters:
+    #         input_collection_name (str): The name of the collection to retrieve the files from.
+    #         title_normalization (bool, default=True): 
+    #             If True, the title will be normalized to prevent issues with special characters.
+
+    #     Returns:
+    #         dict[str,str]: The list of couples title-url to download the PDF files from.
+    #     """
+    #     query_result = self.database[input_collection_name].find()
+    #     result: dict[str,str] = dict()
+
+    #     for record in query_result:
+    #         pdf_URL: str = record["url"]
+    #         pdf_title :str = record["title"]
+
+    #         if title_normalization:
+    #             pdf_title = _title_normalization(pdf_title)
+            
+    #         result.update({pdf_title : pdf_URL})
+        
+    #     return result
+
+    def get_all_records(self, input_collection_name: str) -> list[dict[any]]:
         """
         Retrieves all the records in the given Mongo collection.
 
         Parameters:
             input_collection_name (str): The name of the collection to retrieve the files from.
-            title_normalization (bool, default=True): 
-                If True, the title will be normalized to prevent issues with special characters.
 
         Returns:
-            dict[str,str]: The list of couples title-url to download the PDF files from.
+            list[dict[any]]: The list of records in the collection.
         """
-        query_result = self.database[input_collection_name].find()
-        result: dict[str,str] = dict()
-
-        for record in query_result:
-            pdf_URL: str = record["url"]
-            pdf_title :str = record["title"]
-
-            if title_normalization:
-                pdf_title = _title_normalization(pdf_title)
-            
-            result.update({pdf_title : pdf_URL})
-        
-        return result
+        return list(self.database[input_collection_name].find())
     
     #TODO(unscheduled) consider to implement (it must include url, title, pages and authors) [it will use "insert_record_using_JSON"]
     # def insert_record_using_params(...):
