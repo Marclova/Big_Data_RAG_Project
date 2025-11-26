86e96c40e466a3d35e6b69542e7d50f8c6e43d24
Marclova <cocilovamarco01@gmail.com>
2025-11-26 11:39:15 +0100

    []

--- src/services/db_services/RAG_DB_operators.py
+++ src/services/db_services/RAG_DB_operators.py
@@ -1,11 +1,11 @@
 from typing import override
-from pinecone import (IndexModel, Pinecone as PineconeClient, QueryResponse, ScoredVector, SearchQuery, UpsertResponse)
-from pinecone.db_data import Index
+from pinecone import (Pinecone as PineconeClient, SearchQuery, UpsertResponse, Vector)
+from pinecone.db_data import IndexAsyncio
 from pinecone.core.openapi.db_data.model.search_records_response import SearchRecordsResponse
 from pinecone.core.openapi.db_data.model.search_records_response_result import SearchRecordsResponseResult
 from pinecone.core.openapi.db_data.model.hit import Hit
 
-from src.common.constants import Featured_RAG_DB_engines_enum as RAG_DB_enum
+from src.common.constants import Featured_RAG_DB_engines_enum as RAG_DB
 
 from src.services.db_services.interfaces.DB_operator_interfaces import RAG_DB_operator_I
 
@@ -16,53 +16,23 @@ from models.data_models import RAG_DTModel
  Service module to manage the connection and operations on a database meant to store embedded data for argument retrieval.
  Selected DBs are Pinecone and MongoDB
 """
-#TODO(testing): The whole module needs testings; it may not work
-#TODO(improvement): use an enum for embedder names
-
-def create_new_RAG_index(DB_engine: RAG_DB_enum, embedder_name: str, 
-                         api_key: str, host: str, new_index_name: str) -> bool:
-    """
-    Utility class method used to create a new index even before the initialization of a DB operator,
-        which can't be initialized without a valid index ready.
-    
-    Parameters:
-        DB_engine (RAG_DB_enum): The DB engine featured by the new index to create.
-        embedder_name (str): The embedder used for text content conversion into search vectors.
-        api_key (str): The api key for authentication and permission for the provider's API services.
-        host (str): The host url from where it is possible to interact with the indexes.
-        new_index_name (str): The identifying and immutable name to assign to the new index.
-    Returns:
-        bool: The overall operation outcome.    
-    """
-    try:
-        if DB_engine == RAG_DB_enum.PINECONE:
-            PineconeClient(api_key=api_key, host=host).create_index_for_model(
-                                                            name = new_index_name,
-                                                            embed={
-                                                                "model":embedder_name, # es. "pinecone-sparse-english-v0"
-                                                                "field_map":{"text": "chunk_text"}
-                                                            }
-                                                        )
-        else:
-            raise ValueError(f"RAG index creation for {DB_engine} is not supported")
-    except Exception as e:
-        print(f"Error while trying to create index: {e}")  #TODO(polishing): consider another logging method
 
 
-
+#TODO: implement
 class RAG_PineconeDB_operator(RAG_DB_operator_I):
     """
     Class to manage the Pinecone connection and operations for RAG vector storage.
     """
     def __init__(self, api_key: str, host: str):
-        self.database: PineconeClient
+        # self.database: PineconeClient
+        self.database: IndexAsyncio
+        # self.host: str
 
         self.open_connection(api_key, host)
 
 
-    #region aborted 'get_record_using_embedded_text' method
     # @override
-    # def get_record_using_embedded_text(self, target_index_name: str, embedded_text_to_find: str) -> RAG_DTModel:
+    # async def get_record_using_embedded_text(self, target_index_name: str, embedded_text_to_find: str) -> RAG_DTModel:
     #     # index: IndexAsyncio = PineconeClient.IndexAsyncio(host=self.host)
     #     response: SearchRecordsResponse = await self.database.search(namespace=target_index_name, 
     #                                                                  query=SearchQuery(inputs={"text": embedded_text_to_find}, 
@@ -81,29 +51,24 @@ class RAG_PineconeDB_operator(RAG_DB_operator_I):
             #                    pages=retrieved_record["metadata"]["pages"], 
             #                    authors=retrieved_record["metadata"]["author"]
             #                   )
-    #endregion aborted 'get_record_using_embedded_text' method
 
 
     @override
-    def insert_record(self, target_index_name: str, data_model: RAG_DTModel) -> bool:
-        if(not self.database.has_index(target_index_name)):
-            print(f"ERROR: There's no index named '{target_index_name}'") #TODO(polishing): consider another logging method
-            return False
-        if(self._is_ID_already_in_use(target_index_name, data_model.id)): #False if record already exists
+    async def insert_record(self, target_index_name: str, data_model: RAG_DTModel) -> bool:
+        #There must be no match
+        if self._is_ID_already_in_use(target_index_name, data_model.id):
             return False
         
-        return self._insert_or_update_RAGDTModel(target_index_name, data_model)
+        return self._upsert_record(target_index_name, data_model)
 
 
     @override
-    def update_record(self, target_index_name: str, data_model: RAG_DTModel) -> bool:
-        if(not self.database.has_index(target_index_name)):
-            print(f"ERROR: There's no index named '{target_index_name}'") #TODO(polishing): consider another logging method
-            return False
-        if(not self._is_ID_already_in_use(target_index_name, data_model.id)): #False if record doesn't exist
+    async def update_record(self, target_index_name: str, data_model: RAG_DTModel) -> bool:
+        #There must be a match
+        if not (self._is_ID_already_in_use(target_index_name, data_model.id)):
             return False
         
-        return self._insert_or_update_RAGDTModel(target_index_name, data_model)
+        return self._upsert_record(target_index_name, data_model)
 
 
     # @override
@@ -112,60 +77,31 @@ class RAG_PineconeDB_operator(RAG_DB_operator_I):
 
 
     @override
-    def retrieve_embeddings_from_vectors(self, target_index_name: str, query_vector: list[float], top_k: int) -> list[RAG_DTModel]:
-        if(not self.database.has_index(target_index_name)):
-            print(f"ERROR: There's no index named '{target_index_name}'") #TODO(polishing): consider another logging method
-            return []
-
-        index: Index = self.database.Index(target_index_name)
-        response: QueryResponse = index.query(namespace=target_index_name, 
-                                                      vector=query_vector, top_k=top_k)
-        return self._from_QueryResponse_to_RAGDTModel(response)
+    async def retrieve_vectors(self, target_index_name: str, query_vector: list[float], top_k: int) -> list[RAG_DTModel]:
+        response: SearchRecordsResponse = await self.database.query(namespace=target_index_name, 
+                                                                    vector=query_vector, top_k=top_k)
+        return self._from_SearchRecordsResponse_to_RAGDTModelList(response)
 
 
     @override
     def open_connection(self, api_key: str, host: str):
-        self.database = PineconeClient(api_key=api_key, host=host)
+        client = PineconeClient(api_key)
+        self.database = client.IndexAsyncio(host=host)
+        # self.host = host
 
 
     @override
     def close_connection(self):
-        self.database = None
+        self.database.close()
 
 
     @override
     def get_engine_name(self) -> str:
-        return RAG_DB_enum.PINECONE
-    
-
-    @override
-    def get_embedder_name(self, target_index_name: str) -> str | None:
-        if(not self.database.has_index(target_index_name)):
-            print(f"ERROR: No such index named '{target_index_name}'") #TODO(polishing): consider another logging method
-            return None
-
-        index_model: IndexModel = self.database.describe_index(target_index_name)
-        index_dict: dict[str,any] = index_model.to_dict()
-        spec: dict[str,any] = index_dict.get("spec")
-        embedder_name: str
-
-        if(spec is None):
-            print(f"ERROR: The index '{target_index_name}' has NO model-backed configuration. "
-                  "This implementation only supports indexes with an integrated embedder. "
-                  "Please select an index configured to use one of the featured embedders.")
-            return None
-        try:
-            embedder_name = spec["model"]["embed"]["model"]
-        except Exception as e:
-            print("ERROR: Could not extract embedder name from index config. "
-                  "The index may not be model-backed or has an unexpected structure. "
-                  f"Raised error content: {e}")
-            return None
-        return embedder_name
+        return RAG_DB.PINECONE
     
 
 
-    def _insert_or_update_RAGDTModel(self, target_index_name: str, data_model: RAG_DTModel) -> bool:
+    async def _upsert_record(self, target_index_name: str, data_model: RAG_DTModel) -> bool:
         """
         Private method actually performing the insert/update operation through the 'upsert' Pinecone method.
         Parameters:
@@ -175,9 +111,8 @@ class RAG_PineconeDB_operator(RAG_DB_operator_I):
             bool: True if the a new record has been inserted or if an old one has been updated. 
                     False if the DB has not been changed.
         """
-        index: Index = self.database.Index(target_index_name)
-        # response = self.database.upsert(namespace=target_index_name, vectors=[data_model.generate_JSON_data()])
-        response: UpsertResponse = index.upsert_records(target_index_name, [data_model.generate_JSON_data()])
+        # vector: Vector = self._from_RAGDTModel_to_Vector(data_model)
+        response: UpsertResponse = await self.database.upsert(namespace=target_index_name, vectors=[data_model.generate_JSON_data()])
         
         insertion_count: int = getattr(response, "upserted_count", -1)
 
@@ -186,54 +121,35 @@ class RAG_PineconeDB_operator(RAG_DB_operator_I):
         return (insertion_count > 0)
 
 
-    def _is_ID_already_in_use(self, target_index_name: str, id_to_check: str) -> bool:
+    async def _is_ID_already_in_use(self, target_index_name: str, id_to_check: str) -> bool:
         """
         Private method doing a search query in order to check if the given string is assigned to an existing record.
         Parameters:
-            target_index_name (str): The index wherein to perform the search.
-            id_to_check (str): The record ID to find
+            id_to_check (str): The ID to find
         Returns:
             bool: True if the given ID is used. False otherwise.
         """
-        index: Index = self.database.Index(target_index_name)
         retrieved_data: list[RAG_DTModel] = (
                 self._from_SearchRecordsResponse_to_RAGDTModelList(
-                        index.search(namespace=target_index_name, 
-                                     query = SearchQuery(inputs={"id": id_to_check}, top_k=1)
+                        await self.database.search(
+                                namespace=target_index_name, query = SearchQuery(inputs={"id": id_to_check}, top_k=1)
                         )
                 )
         )
         return (retrieved_data.__len__() > 0)
-    
-    #TODO(testing): The 'ScoredVector' data may be not as expected. Can't implement not knowing the data structure.
-    def _from_QueryResponse_to_RAGDTModel(self, response: QueryResponse) -> list[RAG_DTModel]:
-        """
-        Private method to unwrap a QueryResponse response into a raw dataset.
-        Parameters:
-            response (QueryResponse): The response to unwrap.
-        Returns:
-            list[RAG_DTModel]: The data models list retrieved records.
-        """
-        result_list: list[ScoredVector] = getattr(response, "matches")
-        dataModel_list: list[RAG_DTModel] = []
-
-        for result in result_list:
-            metadata: dict[str,any] = result.__getattr__("metadata")
-
-            # dataModel_list.append(RAG_DTModel(vector=result.__getattr__("vector"))
-            #                 )
-        pass
-            
 
 
-    #TODO(testing): The 'hit.fields' data structure may not be as expected
     def _from_SearchRecordsResponse_to_RAGDTModelList(self, response: SearchRecordsResponse) -> list[RAG_DTModel]:
         """
         Private method to unwrap a SearchRecordsResponse response into a raw dataset.
         Parameters:
             response (SearchRecordsResponse): The response to unwrap.
+            searched_value (dict[str, any]): The exact match to check.
+                                                The first element of the tuple is the name of parameter to check, 
+                                                while the second one is the parameter's value.
         Returns:
-            list[RAG_DTModel]: The data models list retrieved records.
+            list[RAG_DTModel]: The data models list retrieved records. 
+                                Empty list if the retrieved_record does not contains an exact match with the searched value.
         """
         result: SearchRecordsResponseResult = response.result
         hit_list: list[Hit] = result.hits
