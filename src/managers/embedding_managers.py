ea60369ade350e07d42225d3e0b25bdee6c21ebf
Marclova <cocilovamarco01@gmail.com>
2025-11-26 15:29:11 +0100

    [UPDATE] Implemented Pinecone embedder (still needs testings)

--- src/managers/embedding_managers.py
+++ src/managers/embedding_managers.py
@@ -1,5 +1,6 @@
 from src.services.chatBot_services.interfaces.chatBot_service_interfaces import ChatBot_I
 from src.services.embedder_services.interfaces.embedder_interfaces import Embedder_I
+from src.services.other_services import scraper_storage_service as webScraper
 
 from models.data_models import RAG_DTModel
 
@@ -14,17 +15,29 @@ class RAG_manager:
         self.embedder = Embedder_I(embedder_model_name, embedder_api_key)
         self.chatBot = ChatBot_I(chatBot_APIKey)
 
-
-    #TODO(before commit): implement method so that the embedder has just the embedding responsibility
-    def generate_embeddings_from_URL(self, file_URL: str) -> list[RAG_DTModel]:
+    #TODO(refinement): If possible, find a way so that the webScraper can gather name, pages and authors from the file
+    def generate_embeddings_from_URL(self, file_URL: str, 
+                                     file_name: str = None, file_pages: str = None, file_authors = None) -> list[RAG_DTModel]:
         """
         Generates a vector from a file by using the embedder.
         Parameters:
             file_URL (str): The URL leading to the file to embed.
+            file_name (str): The expected name of the file to embed (file name detection not implemented, sorry...)
+            file_pages (str): The expected pages count of the file to embed (file name detection not implemented, sorry...)
+            file_authors (str): The expected authors of the file to embed (file name detection not implemented, sorry...)
         Returns:
             list[RAG_DTModel]: The list of resulting embeddings obtained from the file.
         """
-        minimal_embeddings: dict[str, list[float]] = self.embedder.generate_vectors_from_textChunks(file_URL)
+        textChunkList = webScraper.extract_and_partition_text_from_url(file_URL)
+        minimal_embeddings: dict[str, list[float]] = self.embedder.generate_vectors_from_textChunks(textChunkList)
+
+        DTModel_list: list[RAG_DTModel] = []
+        for (text, vector) in minimal_embeddings.items():
+            DTModel_list.append(
+                            RAG_DTModel(vector,self.embedder.get_embedder_name(), file_URL, 
+                                        file_name, file_pages, text, file_authors, id=None)
+                        )
+        return DTModel_list
     
 
     def send_message_with_responseInfo(self, message: str, responseInfo: set[str]) -> str:
