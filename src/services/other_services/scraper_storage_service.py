8a99ca530a3a85d54806313dd683c8736002297d
Marclova <cocilovamarco01@gmail.com>
2026-01-28 15:02:31 +0100

    [CREATE] Implemented a file creation method in the 'scraper_storage_service.py' module.

--- src/services/other_services/scraper_storage_service.py
+++ src/services/other_services/scraper_storage_service.py
@@ -8,7 +8,7 @@ from pathlib import Path
 # from llama_index.core import Document
 
 
-DEFAULT_PATH = "downloadedFiles"
+DEFAULT_PATH = "file_storage"
 
 
 #TODO(UPDATE): If possible, find a way so that the webScraper can gather authors from the file
@@ -35,18 +35,53 @@ def download_file(url: str, download_folder: str = DEFAULT_PATH) -> str:
         #determine file name from URL
         file_name = url.split("/")[-1]
 
-        #create download folder (if it doesn't exist)
-        if not os.path.exists(download_folder):
-            os.makedirs(download_folder)
-        file_path = os.path.join(download_folder, file_name)
+        # #create download folder (if it doesn't exist)
+        # if not os.path.exists(download_folder):
+        #     os.makedirs(download_folder)
+        # file_path = os.path.join(download_folder, file_name)
 
-        #save file
-        with open(file_path, 'wb') as file:
-            file.write(r.content)
+        # #save file
+        # with open(file_path, 'wb') as file:
+        #     file.write(r.content)
+
+        file_path = create_new_txt_file_from_content(content=r.text.splitlines(),
+                                                     file_name=file_name, download_folder=download_folder)
+
         return file_path
     
     except Exception as e:
         raise RuntimeError(f"Error while trying to download file: {e}")
+    
+
+def create_new_txt_file_from_content(content: list[str], 
+                                     file_name: str = "append_file", download_folder: str = DEFAULT_PATH) -> str:
+    """
+    Method to create a new text file from the provided content.
+    The file is saved into the specified folder with the given name.
+    Parameters:
+        content (list[str]): The content to write into the text file.
+        file_name (str): The name of the text file to create (without extension).
+        download_folder (str): The folder where to save the created text file.
+    Returns:
+        str: The path to the created text file.
+    """
+    try:
+        if(content is None):
+            raise ValueError("The provided content is None")
+        
+        #create download folder (if it doesn't exist)
+        if not os.path.exists(download_folder):
+            os.makedirs(download_folder)
+        file_path = os.path.join(download_folder, f"{file_name}.txt")
+
+        #save file
+        with open(file_path, 'w', encoding='utf-8') as file:
+            for line in content:
+                file.write(f"{line}\n")
+        return file_path
+    
+    except Exception as e:
+        raise RuntimeError(f"Error while trying to create text file: {e}")
     
 
 def delete_file(file_path: str):
